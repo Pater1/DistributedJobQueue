a51acbf4d1c0f02cc1aa1d53e2125f2d7d2422c1
Patrick Conner <PConner000@gmail.com>
2020-03-03 14:34:15 -0700

    Added ParallelJob.cs and SerialJob.cs (RippleSerial TBD)

---
+++ DistributedJobQueue/Job/SerialJob.cs
@@ -0,0 +1,41 @@
+﻿using DistributedJobQueue.Client;
+using DistributedJobQueue.Queue;
+using DistributedJobQueue.Requirements;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DistributedJobQueue.Job
+{
+    public struct SerialJob : IJob
+    {
+        public SerialJob(IRequirement requirement, IEnumerable<IJob> serJobs)
+        {
+            this.JobId = default;
+            this.Requirement = requirement;
+            this.SerJobs = serJobs;
+        }
+
+        public Guid JobId { get; set; }
+        public IRequirement Requirement { get; set; }
+        public IEnumerable<IJob> SerJobs { get; set; }
+
+        public async Task<IEnumerable<IJob>> Run()
+        {
+            IJobQueue jobQueue = this.GetDispatchingQueue();
+
+            foreach(IJob job in SerJobs)
+            {
+                if(job.JobId == default)
+                {
+                    job.JobId = Guid.NewGuid();
+                }
+                await jobQueue.TryEnqueueAsync(job);
+                await jobQueue.WaitForCompletionAsync(this, job);
+            }
+
+            return new IJob[0];
+        }
+    }
+}
